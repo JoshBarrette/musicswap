042a11fc55aa0c3fb2c3bd6588bac3c1ba7132ba
Joshua Barrette <Joshua.Barrette@UHS.local>
2024-09-11 13:25:49 -0500

    Initial commit

---
+++ src/main/java/com/musicswap/musicswap/MusicswapApplication.java
@@ -0,0 +1,13 @@
+package com.musicswap.musicswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicswapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicswap/musicswap/MusicswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicswap.musicswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
